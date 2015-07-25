185b0fb17e106dc7fab252d1253f4df0fd1dccdf
Tyler Powers <gr4yscale@gmail.com>
2015-07-25 14:45:33 +0100

    port trippypyramid into the rapid prototyping environment

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ var dat = require('dat-gui');
 domReady(function(){
 
   var params = {
-    speed: 1000
+    speed: 1000,
+    opacity : 0.25,
+    width : 1.0,
+    height : 0.2,
+    boxThickness : 1.0,
+    hueRange : 0.35,
+    hueOffset : 0.3,
+    twistSpeed : 0.15,
+    rotationSpeed : 0.01,
+    lightYPosition : 20
   };
 
   var OrbitViewer = require('three-orbit-viewer')(THREE);
@@ -25,20 +34,62 @@ domReady(function(){
   light.position.set(10, 20, -20);
   app.scene.add(light);
 
-  var geom = new THREE.BoxGeometry(1,1,1);
-  var material = new THREE.MeshLambertMaterial({color: 0x11CCFF});
-  var cube = new THREE.Mesh(geom, material);
 
-  cube.castShadow=true;
+  // cubes
+  var cubes = [];
+  var numCubes = 100;
 
-  app.scene.add(cube);
+  for ( i = 1; i < numCubes; i ++ ) {
+    var width = 24 - ((24 / numCubes) * i);
+    var material = new THREE.MeshLambertMaterial( { color: 0xffffff, opacity: 0.25, transparent: true } );
+
+    var geometry = new THREE.BoxGeometry( width, 0.1, width);
+    var cube = new THREE.Mesh( geometry, material );
+
+    cube.position.y = 0.2 * i;
+
+    app.scene.add(cube);
+    cubes.push(cube);
+  }
+
+ 
+  // render loop
 
   var tickCounter = 0;
   app.on('tick', function(time) {
     tickCounter += (time / params.speed);
-    cube.rotation.y = Math.sin(tickCounter) * 4.0;
+
+    for (var i = (numCubes - 1); i > 0; i-- ) {
+
+      var cube = cubes[i-1];
+
+      cube.rotation.y += ((Math.sin(tickCounter) * (i / numCubes) * params.twistSpeed) - params.rotationSpeed);
+      cube.scale.x = params.width;
+      cube.scale.z = params.width;
+      cube.scale.y = params.boxThickness;
+      cube.position.y = params.height * i;
+
+      // colors!
+      var hue = (i / numCubes) * params.hueRange - params.hueOffset;
+      cube.material.color.setHSL(hue, 1.0, 0.5);
+      cube.material.opacity = params.opacity;
+    }
+
+    light.position.set( 0, params.lightYPosition, 0);
   });
 
+
+  // params GUI
+
   datgui.add(params, "speed", 10, 2000);
+  datgui.add(params, 'opacity', 0.1, 1);
+  datgui.add(params, 'width', 0.001, 10);
+  datgui.add(params, 'height', 0.02, 3).step(0.01);
+  datgui.add(params, 'boxThickness', 0.01, 10);
+  datgui.add(params, 'hueRange', 0.0, 1);
+  datgui.add(params, 'hueOffset', 0, 1).step(0.01);
+  datgui.add(params, 'twistSpeed', 0.0, 0.8);
+  datgui.add(params, 'rotationSpeed', 0.0, 0.1).step(0.01);
+  datgui.add(params, 'lightYPosition', 0.01, 60);
 
 });
