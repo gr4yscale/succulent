3095db7aee62daf619c8dceb054b60e2d020e2eb
Tyler Powers <tyler@unstatic.tech>
2017-08-02 16:57:00 +0100

    drop in succulent build from the other repository, for github pages hosting

--- index.js
+++ index.js
@@ -1,25 +1,105 @@
 var domReady = require('domready');
 var dat = require('dat-gui');
-
 var glslify = require('glslify');
 
-domReady(function(){
+var Succulent = require('./js/succulent')(THREE);
 
+domReady(function(){
   var params = {
     speed: 1000,
-    opacity : 0.25,
-    width : 2.0,
-    height : 0.2,
-    boxThickness : 1.0,
-    hueRange : 0.35,
-    hueOffset : 0.3,
-    twistSpeed : 0.0,
-    rotationSpeed : 0.00,
-    lightYPosition : 80
   };
 
-  var OrbitViewer = require('three-orbit-viewer')(THREE);
+  var boxes = [];
+  var shaders = [];
+  var fragShaders = [];
 
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function findRandomUnusedSucculentPosition(offsetMin, offsetMax, box) {
+    var newBox = box.clone();
+    var offsetX = getRandomArbitrary(offsetMin, offsetMax);
+    var offsetZ = getRandomArbitrary(offsetMin, offsetMax);
+    var offsetVec3 = new THREE.Vector3(offsetX, 0, offsetZ);
+    newBox.translate(offsetVec3);
+
+    for (var i = 0; i < boxes.length; i++) {
+      var aBox = boxes[i];
+      if (newBox.intersectsBox(aBox)) {
+        // console.log('There was an intersection: ' + offsetX + ' ' + offsetZ);
+        var newOffsetMin = offsetMin - 0.02;
+        var newOffsetMax = offsetMax + 0.02;
+        return findRandomUnusedSucculentPosition(newOffsetMin, newOffsetMax, box);
+      }
+    }
+    // console.log('Found the offset that i want to work with: ' + newBox.center().x + ' ' + newBox.center().y + ' ' + boxes.length);
+    return newBox;
+  }
+
+  function addSucculent() {
+    var randomShaderIndex = Math.floor(getRandomArbitrary(0, shaders.length));
+    var shaderMaterial = shaders[randomShaderIndex];
+
+    var succulent = Succulent(shaderMaterial);
+
+    var bboxHelperA = new THREE.BoundingBoxHelper(succulent);
+    bboxHelperA.update();
+    // var bbox = new THREE.Box3().setFromObject(succulent);
+    var newBox = findRandomUnusedSucculentPosition(-0.02, 0.02, bboxHelperA.box);
+
+    succulent.position.x = newBox.center().x;
+    succulent.position.y = 0;
+    succulent.position.z = newBox.center().z;
+
+    var helper = new THREE.BoundingBoxHelper(succulent);
+    helper.update();
+    // helper.visible = true;
+    // app.scene.add(helper);
+    app.scene.add(succulent);
+    boxes.push(helper.box);
+  }
+
+  function loadShaderMaterials() {
+    fragShaders.push(glslify(__dirname + '/shaders/1.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/2.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/3.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/4.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/5.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/6.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/7.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/8.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/9.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/10.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/11.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/12.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/13.frag'))
+    fragShaders.push(glslify(__dirname + '/shaders/14.frag'))
+
+    for (var i = 0; i < fragShaders.length; i++) {
+
+      var fragShaderString = __dirname + '/shaders/1.frag';
+      // console.log(fragShaderString);
+
+      var shaderMaterial = new THREE.ShaderMaterial({
+        uniforms : {
+          iGlobalTime: { type: 'f', value: 0 }
+        },
+        defines: {
+          USE_MAP: ''
+        },
+        vertexShader : glslify(__dirname + '/shaders/passthrough.vert'),
+        fragmentShader : fragShaders[i],
+        // fragmentShader : glslify(__dirname + '/shaders/1.frag'),
+        side: THREE.DoubleSide,
+        // wireframe: true
+      });
+      shaders.push(shaderMaterial);
+    }
+  }
+
+  // var datgui = new dat.GUI();
+  var OrbitViewer = require('three-orbit-viewer')(THREE);
   var app = OrbitViewer({
     clearColor: 'rgb(50,50,50)',
     clearAlpha: 1.0,
@@ -28,151 +108,41 @@ domReady(function(){
       antialias: true,
       alpha: false
     },
-    position: new THREE.Vector3(0, 0.5,-.5),
+    position: new THREE.Vector3(0, 2, -2.5),
     //target: new THREE.Vector3(0,0.5,0)
   });
+  var setupLights = require('./js/lights')(THREE, app.scene);
+  setupLights();
+  loadShaderMaterials();
 
-  var datgui = new dat.GUI();
-
-  var light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-  light.position.set(0, 40, 80);
-
-  var lightB = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-  lightB.position.set(1, 20, 1);
-
-  lightB.castShadow = true;
-  lightB.shadowCameraVisible = true;
-
-  lightB.shadowCameraNear = 1;
-  lightB.shadowCameraFar = 1000;
-  lightB.shadowCameraLeft = -1000;
-  lightB.shadowCameraRight = 1000;
-  lightB.shadowCameraTop = 1000;
-  lightB.shadowCameraBottom = -1000;
-  lightB.distance = 0;
-  lightB.intensity = 1.0;
-
-  //app.scene.add(light);
-  app.scene.add(lightB);
-
-  var spotLight = new THREE.SpotLight();
-  spotLight.angle = Math.PI / 8;
-  spotLight.exponent = 30;
-  spotLight.position.copy(new THREE.Vector3(40, 60, -50));
-
-
-  spotLight.castShadow = true;
-  spotLight.shadowCameraNear = 50;
-  spotLight.shadowCameraFar = 200;
-  spotLight.shadowCameraFov = 35;
-  spotLight.shadowMapHeight = 2048;
-  spotLight.shadowMapWidth = 2048;
-
-  spotLight.name = 'spotLight';
-  app.scene.add(spotLight);
-
-
-  var petals = [];
-  var petalCount = 60;
-  var curveAmountA;
-  var curveAmountB = 0.2; // multiplier for log curvature
-  var curveAmountC = 0.6; // initial curve amount
-  var curveAmountD = 0.2;
-  var layers = 8.0;
-  var petalLength = 0.5;
-  var petalWidth = 0.4;
-
-  // shader
-  var shaderMaterial = new THREE.ShaderMaterial({
-      uniforms : {
-        iGlobalTime: { type: 'f', value: 0 }
-      },
-      defines: {
-        USE_MAP: ''
-      },
-      vertexShader : glslify(__dirname + '/shaders/sketch.vert'),
-      fragmentShader : glslify(__dirname + '/shaders/sketch.frag'),
-      side: THREE.DoubleSide
-  });
-
-  // console.log(shaderMaterial);
-  // console.log(shaderMaterial);
-
-  var material = new THREE.MeshLambertMaterial({
-                  color: 0xFF333FF,
-                  side: THREE.DoubleSide,
-                  shading: THREE.SmoothShading,
-                  //wireframe: true
-                });
-
-
-  var petalFunc = function (u, v) {
-            var curve = Math.pow(u * 4.0, curveAmountD) * curveAmountA; // * (Math.pow(u, 0.9));
-            var petalOutline = (Math.sin((u - 1.5) * 2.0) * Math.sin((v - 0.5) * Math.sin((u + 2.14))) * 2.0);
-            return new THREE.Vector3(petalOutline * petalWidth, u * petalLength, curve);
-        };
-
-  var createPetalMesh = function() {
-    var geom = new THREE.ParametricGeometry(petalFunc, 20, 20);
-    var mesh = new THREE.Mesh(geom, shaderMaterial);
-    return mesh;
+  for (var i = 0; i < 200; i++) {
+    addSucculent()
   }
-
-  for (var i = 0; i < petalCount; i++) {
-    var j = i / petalCount;
-    var rotationAmount = j * layers;
-    //curveAmount = 0.1 + (Math.pow(j, 2.0) * 1.000001);
-    curveAmountA = Math.abs(curveAmountC + (Math.log(j) * curveAmountB));
-    // console.log(curveAmountA);
-    var petalMesh = createPetalMesh();
-
-    petalMesh.rotation.y = THREE.Math.degToRad(rotationAmount * 360);
-
-    var scale = curveAmountA;
-    petalMesh.scale.x = scale;
-    petalMesh.scale.y = scale;
-    petalMesh.scale.z = scale;
-
-    petals.push();
-    app.scene.add(petalMesh);
-  }
-
-  //app.camera.position.x = 0;
-  //app.camera.position.y = 0;
-  //app.camera.position.z = -1;
-
-  //var helper = new THREE.BoundingBoxHelper(mesh, 0xff0000);
-  //helper.update();
-  //app.scene.add(helper);
-
-  //var sphereGeom = new THREE.SphereGeometry(0.01, 10, 10);
-  //var sphereMesh = new THREE.Mesh(sphereGeom, material);
-
-  //app.scene.add(sphereMesh);
 
   // render loop
-
   var tickCounter = 0;
   app.on('tick', function(time) {
     tickCounter += (time / params.speed);
+    shaders[0].uniforms.iGlobalTime.value = tickCounter;
+    shaders[1].uniforms.iGlobalTime.value = tickCounter;
+    shaders[2].uniforms.iGlobalTime.value = tickCounter;
+    shaders[3].uniforms.iGlobalTime.value = tickCounter;
+    shaders[4].uniforms.iGlobalTime.value = tickCounter;
+    shaders[5].uniforms.iGlobalTime.value = tickCounter;
+    shaders[6].uniforms.iGlobalTime.value = tickCounter;
+    shaders[7].uniforms.iGlobalTime.value = tickCounter;
+    shaders[8].uniforms.iGlobalTime.value = tickCounter;
+    shaders[9].uniforms.iGlobalTime.value = tickCounter;
+    shaders[10].uniforms.iGlobalTime.value = tickCounter;
+    shaders[11].uniforms.iGlobalTime.value = tickCounter;
+    shaders[12].uniforms.iGlobalTime.value = tickCounter;
+    shaders[13].uniforms.iGlobalTime.value = tickCounter;
 
-    shaderMaterial.uniforms.iGlobalTime.value = tickCounter;
-    // console.log(app.camera.position);
     //light.position.set( 0, params.lightYPosition, 0);
   });
 
-
   // params GUI
-
-  //datgui.add(params, "speed", 10, 2000);
-  //datgui.add(params, 'opacity', 0.1, 1);
-  //datgui.add(params, 'width', 0.001, 10);
-  //datgui.add(params, 'height', 0.02, 3).step(0.01);
-  //datgui.add(params, 'boxThickness', 0.01, 10);
-  //datgui.add(params, 'hueRange', 0.0, 1);
-  //datgui.add(params, 'hueOffset', 0, 1).step(0.01);
-  //datgui.add(params, 'twistSpeed', 0.0, 0.08);
-  //datgui.add(params, 'rotationSpeed', 0.0, 0.1).step(0.01);
-  //datgui.add(params, 'lightYPosition', 0.01, 60);
+  // datgui.add(params, "speed", 10, 2000);
+//datgui.add(params, 'height', 0.02, 3).step(0.01);
 
 });
